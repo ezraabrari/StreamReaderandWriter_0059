c80a5df920b25271d323374b5bfbad5d26dc7b9b
ezraabrari <ezraabrari13@gmail.com>
2024-06-03 14:50:02 +0700

    Membuat looping untuk menulis dan ketika mengetik q akan break/berhenti

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,11 +10,20 @@ int main() {
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
 
+    // Membuka file dalam mode menulis
     ofstream outfile;
-
+    // Menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis File, \'q\' untuk keluar" << endl;
-
-    
+    // Unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        // Mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // Menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
