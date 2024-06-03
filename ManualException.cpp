8a107f41c739c068169a925b60dbeaf48f8e2cd4
ezraabrari <ezraabrari13@gmail.com>
2024-06-03 15:21:09 +0700

    membenarkan trowh

--- ManualException.cpp
+++ ManualException.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     try {
         cout << "Selamat belajar di prodi TI UMY " << endl;
-        //Throw 0.5; // melemparkan sebuah integer maka
+        //throw 0.5; // melemparkan sebuah integer maka
         cout << "pernyataan tidak akan dieksekusi" << endl;
     }
 
