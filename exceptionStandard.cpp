439c3cfbcb6cbd3587d73eeccb22c02328dd44d6
ezraabrari <ezraabrari13@gmail.com>
2024-06-03 15:16:40 +0700

    Penanda bahwa telah selesa

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main () {
-    cout << "awal program" << endl; // penanda 1:
+    cout << "awal program" << endl; // penanda 1:....
     try {
         array<int, 3>data = {8, 4, 2};
         // pesan array interger 3 element
@@ -18,5 +18,9 @@ int main () {
         cout << e.what() << endl;
         /*akan dieksekusi karna array data hanya memilki 3 elemen*/
     }
-    
+    cout << "baris Program yang terakhir" << endl;
+    /*Penanda 2: bahwa program berjalan tanpa henti meskipun terjadi kesalahan*/
+
+    return 0;
+
 }
