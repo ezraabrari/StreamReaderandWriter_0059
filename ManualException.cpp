1ababea528322ef5d4dea8530eebdfe3bca1dd64
ezraabrari <ezraabrari13@gmail.com>
2024-06-03 15:03:01 +0700

    Membuat try

---
+++ ManualException.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        cout << "Selamat belajar di prodi TI UMY " << endl;
+        //Throw 0.5; // melemparkan sebuah integer maka
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+
+    
+}
